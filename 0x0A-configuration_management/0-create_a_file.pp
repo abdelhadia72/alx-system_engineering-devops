520a2557f61279aa41ede734137534c55a8bce12
abdelhadia72 <thisisavnb@gmail.com>
2023-12-22 21:58:47 +0100

    Fixing task 0 (zz)

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Using Puppet, create a file in /tmp.
+# 0-create_a_file.pp in /tmp
 
 file { '/tmp/school':
-    ensure  => 'file',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet',
+  ensure  => 'file',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
