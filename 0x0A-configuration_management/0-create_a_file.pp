46560a521ec999594295cce8d4150d978265aeee
abdelhadia72 <thisisavnb@gmail.com>
2023-12-22 11:51:16 +0100

    fixing 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # Using Puppet, create a file in /tmp.
 file { '/tmp/school':
   ensure => file,
-  mode => '0774'
+  mode => '0774',
   owner => 'www-data',
   group => 'www-data',
   content => 'I love Puppet',
 }
+
