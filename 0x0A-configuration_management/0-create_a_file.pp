734669fed55b46bee5343f60dd68067c4bae92ac
abdelhadia72 <thisisavnb@gmail.com>
2023-12-22 18:08:37 +0100

    Fixing 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# Using Puppet, create a school file in /tmp.
+# Using Puppet, create a file in /tmp.
 file { '/tmp/school':
-  mode    => '0774',
-  owner   => 'www-data',
-  group   => 'www-data',
+  ensure => file,
+  mode => '0774',
+  owner => 'www-data',
+  group => 'www-data',
   content => 'I love Puppet',
 }
-
