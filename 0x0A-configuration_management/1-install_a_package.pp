9e0920fa30767414270465635087d44d8bcb9412
abdelhadia72 <thisisavnb@gmail.com>
2023-12-22 11:55:30 +0100

    fixing 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,9 @@
 # Using Puppet, install flask from pip3.
 
+
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => '/usr/bin',
   unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
+
