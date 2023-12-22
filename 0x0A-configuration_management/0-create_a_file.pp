f5617ede5486842ba3f74ce1303af99fcbe46f8a
abdelhadia72 <thisisavnb@gmail.com>
2023-12-22 18:43:49 +0100

    Fixing Task 0.4

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # Using Puppet, create a file in /tmp.
 
 file { '/tmp/school':
-    owner   => 'www-data',
+    ensure  => file,
     mode    => '0744',
+    owner   => 'www-data',
     group   => 'www-data',
     content => 'I love Puppet',
 }
