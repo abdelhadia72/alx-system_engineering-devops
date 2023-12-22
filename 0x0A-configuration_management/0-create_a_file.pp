0a08a429a698c5d437defecc817e4107188c61d9
abdelhadia72 <thisisavnb@gmail.com>
2023-12-22 18:26:15 +0100

    Fixing 0.1

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
 file { '/tmp/school':
-  ensure  => file,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+	owner   => 'www-data',
+	mode    => '0744',
+	group  => 'www-data',
+	content => 'I love Puppet'
 }
