8f18f54577ead0de41e2f713a751d7db83aba6a4
abdelhadia72 <thisisavnb@gmail.com>
2024-01-19 18:38:35 +0100

    Fix talk 1 in CM

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,7 +2,7 @@
 
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
   path    => '/usr/bin',
   unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
