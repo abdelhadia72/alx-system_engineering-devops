6682c2c98032aa26c4e6c472590d87f07868daed
abdelhadia72 <thisisavnb@gmail.com>
2024-01-29 19:00:19 +0100

    Task 1 (try 5)

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,14 +12,20 @@ from sys import argv
 if __name__ == "__main__":
     r1 = get('https://jsonplaceholder.typicode.com/todos/')
     tasks = r1.json()
-    r1 = get(f'https://jsonplaceholder.typicode.com/users/{int(argv[1])}')
-    user = r1.json()
+    username = ""
+    r1 = get(f'https://jsonplaceholder.typicode.com/users/')
+    users = r1.json()
 
     if os.path.exists and os.path.isfile(f"{argv[1]}.csv"):
         os.remove(f"{argv[1]}.csv")
 
+    for user in users:
+        if user.get("id") == int(argv[1]):
+            username = user.get("username")
+            break
+
     with open(f"{argv[1]}.csv", "a", encoding="utf-8") as f:
         for task in tasks:
             if task.get("userId") == int(argv[1]):
-                f.write(f'"{argv[1]}","{user.get("username")}","{
+                f.write(f'"{argv[1]}","{username}","{
                         task.get("completed")}","{task.get("title")}"\n')
