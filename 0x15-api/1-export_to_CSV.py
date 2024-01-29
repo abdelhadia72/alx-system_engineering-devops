24385eb5c6b6b778c7dd54d7a0576235ce1dc3c6
abdelhadia72 <thisisavnb@gmail.com>
2024-01-29 18:45:33 +0100

    export data for user in id.csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,19 @@
+"""
+This script exports user tasks from the JSONPlaceholder API to a CSV file.
+The script takes a user ID as a command-line argument and retrieves the tasks associated with that user.
+It then creates a CSV file with the following columns: "USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE".
+Each row in the CSV file represents a task associated with the specified user.
+
+Usage: python3 1-export_to_CSV.py [user_id]
+"""
+
+from requests import get
+from sys import argv
+import os
+
+# Rest of the code...
 #!/usr/bin/python3
 
-"""
-Python script that, using a REST API, for a given employee ID,
-returns information about his/her TODO list progress.
-"""
 
 from requests import get
 from sys import argv
@@ -23,8 +33,3 @@ if __name__ == "__main__":
             if task.get("userId") == int(argv[1]):
                 f.write(f'"{argv[1]}","{user.get("username")}","{
                         task.get("completed")}","{task.get("title")}"\n')
-
-"""
-"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-"2","Antonette","False","laborum aut in quam"
-"""
