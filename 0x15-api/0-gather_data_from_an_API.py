0691859ee89e758d0becb3bc305bfd9f4416f624
abdelhadia72 <thisisavnb@gmail.com>
2024-01-29 16:44:07 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,20 +5,22 @@
     and who own this tasks
 """
 
+import requests
+import sys
 
 if __name__ == "__main__":
 
-    import requests
-    import sys
-
+    """ check for argv """
     if len(sys.argv) != 2:
         print('Using python3 file.py <userId>')
         sys.exit(1)
 
+    """ fetch tasks and user """
     todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
     user_name = requests.get(
         f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}').json()
 
+    """ count the tasks """
     all_tasks = 0
     done_tasks = 0
     for task in todos:
@@ -29,6 +31,7 @@ if __name__ == "__main__":
             else:
                 all_tasks = all_tasks + 1
 
+    """ print out tasks by the user """
     print(
         f"First line: Employee {
             user_name['name']} is done with tasks({done_tasks}/{all_tasks}):")
