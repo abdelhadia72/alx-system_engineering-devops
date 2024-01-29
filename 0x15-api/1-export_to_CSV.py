c5a4ac59bd0d91b6226ae3849bdd35af99b420b9
abdelhadia72 <thisisavnb@gmail.com>
2024-01-29 19:30:45 +0100

    with style

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,27 +6,37 @@ Usage: python3 1-export_to_CSV.py [user_id]
 """
 
 import os
-import requests
+from requests import get
 from sys import argv
 
-def fetch_user_tasks(user_id):
-    tasks_response = requests.get(f'https://jsonplaceholder.typicode.com/todos/')
-    tasks = tasks_response.json()
 
-    users_response = requests.get(f'https://jsonplaceholder.typicode.com/users/')
+def fetch_user_tasks(user_id):
+    tasks_res = get(f'https://jsonplaceholder.typicode.com/todos/')
+    tasks = tasks_res.json()
+
+    users_response = get(f'https://jsonplaceholder.typicode.com/users/')
     users = users_response.json()
 
-    username = next((user.get("username") for user in users if user.get("id") == int(user_id)), "")
+    username = next(
+        (user.get("username")
+            for user in users
+            if user.get("id") == int(user_id)),
+        ""
+    )
 
     csv_filename = f"{user_id}.csv"
 
     if os.path.exists(csv_filename):
         os.remove(csv_filename)
 
-    with open(csv_filename, "a", encoding="utf-8") as csv_file:
+    task = task.get("completed")
+    title = task.get("title")
+
+    with open(csv_filename, "a", encoding="utf-8") as f:
         for task in tasks:
             if task.get("userId") == int(user_id):
-                csv_file.write(f'"{user_id}","{username}","{task.get("completed")}","{task.get("title")}"\n')
+                f.write(f'"{user_id}","{username}","{task}","{title}"\n')
+
 
 if __name__ == "__main__":
     if len(argv) != 2:
