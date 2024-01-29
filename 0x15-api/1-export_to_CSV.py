23a8308756139ef1d5c7650bcffe3a5662dd4306
abdelhadia72 <thisisavnb@gmail.com>
2024-01-29 19:08:24 +0100

    no style

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,18 +9,14 @@ import os
 import requests
 from sys import argv
 
-
 def fetch_user_tasks(user_id):
-    tasks_response = requests.get(
-        f'https://jsonplaceholder.typicode.com/todos/')
+    tasks_response = requests.get(f'https://jsonplaceholder.typicode.com/todos/')
     tasks = tasks_response.json()
 
-    users_response = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/')
+    users_response = requests.get(f'https://jsonplaceholder.typicode.com/users/')
     users = users_response.json()
 
-    username = next((user.get("username")
-                    for user in users if user.get("id") == int(user_id)), "")
+    username = next((user.get("username") for user in users if user.get("id") == int(user_id)), "")
 
     csv_filename = f"{user_id}.csv"
 
@@ -30,11 +26,7 @@ def fetch_user_tasks(user_id):
     with open(csv_filename, "a", encoding="utf-8") as csv_file:
         for task in tasks:
             if task.get("userId") == int(user_id):
-                csv_file.write(
-                    f'"{user_id}","{username}","{
-                        task.get("completed")}","{
-                        task.get("title")}"\n')
-
+                csv_file.write(f'"{user_id}","{username}","{task.get("completed")}","{task.get("title")}"\n')
 
 if __name__ == "__main__":
     if len(argv) != 2:
