3aa0cd48661035b8888095c4b8f8c9c2a1d121b3
abdelhadia72 <thisisavnb@gmail.com>
2024-01-29 16:32:11 +0100

    fix style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,17 +4,17 @@
 import requests
 import sys
 
-# check for argv
+""" check for argv """
 if len(sys.argv) != 2:
     print('Using python3 file.py <userId>')
     sys.exit(1)
 
-# fetch tasks and user
+""" fetch tasks and user """"
 todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
 user_name = requests.get(
     f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}').json()
 
-# count the tasks
+""" count the tasks """
 all_tasks = 0
 done_tasks = 0
 for task in todos:
@@ -26,10 +26,10 @@ for task in todos:
             all_tasks = all_tasks + 1
 
 
-# print out tasks by the user
+""" print out tasks by the user """
 print(
     f"First line: Employee {
         user_name['name']} is done with tasks({done_tasks}/{all_tasks}):")
 for task in todos:
-    if task['userId'] == int(sys.argv[1]) and task['completed'] == True:
+    if task['userId'] == int(sys.argv[1]) and task['completed']:
         print(task['title'])
