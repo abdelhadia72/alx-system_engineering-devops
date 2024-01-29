ace5bfe0c2afbd2068363b83c00053145fe6b3b8
abdelhadia72 <thisisavnb@gmail.com>
2024-01-29 17:07:33 +0100

    trun it into function

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,12 +10,14 @@ Usage:
 import requests
 import sys
 
+
 def fetch_tasks(user_id):
     """Fetch tasks from the API based on user ID."""
     todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
     user_name = requests.get(
         f'https://jsonplaceholder.typicode.com/users/{user_id}').json()
     return todos, user_name
+
 
 def count_tasks(todos, user_id):
     """Count completed and all tasks for the user."""
@@ -28,12 +30,16 @@ def count_tasks(todos, user_id):
             all_tasks += 1
     return all_tasks, done_tasks
 
+
 def print_tasks(user_name, all_tasks, done_tasks, todos):
     """Print tasks completed by the user."""
-    print(f"Employee {user_name['name']} is done with tasks({done_tasks}/{all_tasks}):")
+    print(
+        f"Employee {
+            user_name['name']} is done with tasks({done_tasks}/{all_tasks}):")
     for task in todos:
         if task['userId'] == user_id and task['completed']:
             print(f"\t{task['title']}")
+
 
 if __name__ == "__main__":
     """Check for argv."""
@@ -45,4 +51,3 @@ if __name__ == "__main__":
     todos, user_name = fetch_tasks(user_id)
     all_tasks, done_tasks = count_tasks(todos, user_id)
     print_tasks(user_name, all_tasks, done_tasks, todos)
-
