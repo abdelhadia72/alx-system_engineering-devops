507cd4a7c46c85603f7e91260f400e01215a8971
abdelhadia72 <thisisavnb@gmail.com>
2024-02-22 20:21:26 +0100

    pp2

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,9 @@
+# fix it`:p` 
+exec { 'hard limit':
+  command => "sed -i 's/5/4000/' /etc/security/limits.conf",
+  path    => '/bin'
+}
+exec { 'soft limit':
+  command => "sed -i 's/4/2000/' /etc/security/limits.conf",
+  path    => '/bin'
+}
